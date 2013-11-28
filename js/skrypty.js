8301a2521a4426932d3a13abd05f38a0d315db33
piernik <lpiernik@interia.pl>
2013-11-28 13:50:54 +0100

    atrakcja.html

--- js/skrypty.js
+++ js/skrypty.js
@@ -1,42 +1,71 @@
 Site = {
 	atrakcje : null,
+	id_atrakcji:null,
+	atrakcja:null,
 };
 //localStorage.removeItem( 'atrakcje');
 Site.init = function() {
-	$.support.cors=true;
+	$.support.cors = true;
 	$.mobile.allowCrossDomainPages = true;
-	$.mobile.defaultPageTransition  = 'flip';
+	$.mobile.defaultPageTransition = 'flip';
 	if (!localStorage.atrakcje) {
 		//alert('Wgrywam dane z serwera');
 		Site.wczytajDane();
 	} else {
 		//alert('Mam dane lokalnie');
 		//if (plikURL()=='index.html') {
-			//$.mobile.navigate( "main.html" );
+		//$.mobile.navigate( "main.html" );
 		//} else {
-			//alert(localStorage.atrakcje);
-			this.atrakcje=JSON.parse(localStorage.atrakcje);
-			this.generatePolecamy();
-			this.generateList();
+		//alert(localStorage.atrakcje);
+		this.atrakcje = JSON.parse(localStorage.atrakcje);
+		this.generatePolecamy();
+		this.generateList();
 		//}
 	}
-	$( window ).resize(function() {
+	$(document).on('pagebeforeshow', function () {
+		Site.id_atrakcji=localStorage.id;
+		localStorage.removeItem('id');
+		if (Site.id_atrakcji) {
+			Site.pokazAtrakcje();
+		}
+  }); 
+	
+	$("a").on("click", function(event) {
+		event.preventDefault();
+		var href=$(this).attr("href");
+		
+		var url = href.split("?");
+		var parameters = url[1].split("&");
+		for (var p in parameters) {
+			var s=parameters[p].split("=");
+			localStorage[s[0]] = s[1];
+			//alert(localStorage.id);
+		}
+		$.mobile.navigate(url[0]);
+	});
+	$(window).resize(function() {
 		resize();
 	});
 	resize();
-	var szer=$( "div[data-role='okno']" , $("div[data-role='ekran']")).length*$( "div[data-role='okno']").outerWidth(true);
-	$("div[data-role='ekran']").width(szer+'px');
-	$("div[data-role='wnetrze']").height(($("div[data-role='okno']").height()-$("div[data-role='okno'] h2").outerHeight(true)-50)+'px');
+	var szer = $("div[data-role='okno']", $("div[data-role='ekran']")).length * $("div[data-role='okno']").outerWidth(true);
+	$("div[data-role='ekran']").width(szer + 'px');
+	$("div[data-role='wnetrze']").height(($("div[data-role='okno']").height() - $("div[data-role='okno'] h2").outerHeight(true) - 50) + 'px');
+};
+Site.pokazAtrakcje=function() {
+	this.atrakcja=this.atrakcje[this.id_atrakcji];
+	$("div[data-role='header'] h1").html(this.atrakcja.tytul);
 };
 Site.generatePolecamy = function() {
-	var i=0;
+	var i = 0;
 	for (a in this.atrakcje) {
 		if (this.atrakcje[a].wyrozniony) {
 			i++;
-			var inner = $('<a><img src="' + this.atrakcje[a].zdjecia.lista_glowna + '"> ' + '<h2>' + this.atrakcje[a].tytul + '</h2></a>');
+			var inner = $('<a href="atrakcja.html?id=' + a + '"><img src="' + this.atrakcje[a].zdjecia.lista_glowna + '"> ' + '<h2>' + this.atrakcje[a].tytul + '</h2></a>');
 			var div = $("<div></div>").append(inner);
-			if (i%2==0) var cl="grid-1";
-			else var cl="grid-2";
+			if (i % 2 == 0)
+				var cl = "grid-1";
+			else
+				var cl = "grid-2";
 			$(div).addClass(cl);
 			$(".polecamy").append(div);
 		}
@@ -45,13 +74,13 @@ Site.generatePolecamy = function() {
 Site.generateList = function() {
 	for (a in this.atrakcje) {
 		var inner = $('<img src="' + this.atrakcje[a].zdjecia.lista + '"> ' + '<h2>' + this.atrakcje[a].tytul + '</h2>');
-		var a = $("<a></a>").append(inner);
+		var a = $('<a href="atrakcja.html?id=' + a + '"></a>').append(inner);
 		var li = $("<li></li>").append(a);
 		$("#listaAtrakcji").append(li);
 	}
 	$("#listaAtrakcji").listview("refresh");
 };
-Site.wczytajDane = function () {
+Site.wczytajDane = function() {
 	$.mobile.loading("show", {
 		text : "Wczytuję",
 		textVisible : true,
@@ -64,7 +93,7 @@ Site.wczytajDane = function () {
 	}).done(function(dane) {
 		//alert(dane);
 		//alert('mam');
-		
+
 		//$(".strona").show();
 		//$(".wgrywam").hide('fast');
 		localStorage.atrakcje = JSON.stringify(dane);
@@ -75,10 +104,12 @@ Site.wczytajDane = function () {
 	});
 };
 function resize() {
-	$("div[data-role='content']").height(($( window ).height())+'px');
+	$("div[data-role='content']").height(($(window).height()) + 'px');
 }
+
 function plikURL() {
-    var loc = window.location;
-    var pathName = loc.pathname.substring(loc.pathname.lastIndexOf('/') + 1);
-    return pathName;//loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+	var loc = window.location;
+	var pathName = loc.pathname.substring(loc.pathname.lastIndexOf('/') + 1);
+	return pathName;
+	//loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
 }
