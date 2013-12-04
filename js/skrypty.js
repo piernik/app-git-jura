56951ce348f8d7c4f8c65bb4ddd23c97ddfa9674
piernik <lpiernik@interia.pl>
2013-12-04 13:55:08 +0100

    czysty kod

--- js/skrypty.js
+++ js/skrypty.js
@@ -29,45 +29,76 @@ Site = {
 		22 : "Punkty na szlakach"
 	},
 	atrakcjeWgRodzajow : {},
+	params:{},
 };
 localStorage.removeItem('back');
 localStorage.removeItem('rodzaj');
 localStorage.removeItem('nazwa');
-localStorage.removeItem('atrakcje');
-$( document ).one( "pagecreate", ".index", function() {
-	console.log("create index");
+//localStorage.removeItem('atrakcje');
+$(document).one("pageshow", ".index", function() {
+	//console.log("show index");
 	if (!localStorage.atrakcje) {
 		Site.wczytajDane();
 	} else {
-		this.atrakcje = JSON.parse(localStorage.atrakcje);
-		this.generatePolecamy();
-		this.generateList();
+		//console.log("mam dane");
+		Site.atrakcje = JSON.parse(localStorage.atrakcje);
+		Site.przygotujDane();
+		Site.generatePolecamy();
+		Site.generateList();
+	}
+});
+$(document).on("pageshow", ".atrakcje", function() {
+	//console.log("show atrakcje");
+	Site.generateList("atrakcjeListaAtrakcji");
+	Site.filtrujWyniki();
+	var option = $("<option value=''>Wszystkie</option>");
+	$("select#rodzaj").append(option);
+	for (var r in Site.rodzaje) {
+		if (Site.atrakcjeWgRodzajow[r]) {
+			var option = $("<option value='" + r + "'>" + Site.rodzaje[r] + " (" + Site.atrakcjeWgRodzajow[r].length + ")</option>");
+			$("select#rodzaj").append(option);
+		}
+	}
+	if (localStorage.back) {
+		if (localStorage.rodzaj)
+			$("select#rodzaj").val(localStorage.rodzaj);
+		if (localStorage.nazwa)
+			$("input#nazwa").val(localStorage.nazwa);
+		Site.filtrujWyniki();
+	} else {
+		if (Site.params.rodzaj)
+			$("select#rodzaj").val(Site.params.rodzaj);
+	}
+	$("select#rodzaj").selectmenu('refresh');
+	$("select#rodzaj").change(function() {
+		Site.filtrujWyniki();
+	});
+	$("input#nazwa").keyup(function() {
+		Site.filtrujWyniki();
+	});
+});
+$(document).on("pageshow", ".atrakcja", function() {
+	//console.log("show atrakcja");
+	if (Site.params.id) {
+		Site.id_atrakcji=Site.params.id;
+		Site.wczytajDaneAtrakcji();
 	}
 });
 Site.init = function() {
-	console.log("init");
+	//console.log("init");
 	$.support.cors = true;
 	$.mobile.allowCrossDomainPages = true;
 	$.mobile.defaultPageTransition = 'slide';
 	$.mobile.orientationChangeEnabled = false;
-	
-	/*
-	$("body").on("swipeleft", function(event) {
-		console.log("swipe left");
-		$("div[data-role='content']").animate({scrollLeft: pos - 200} );
+
+	$(document).on("pageshow", function() {
+		//console.log("global pageshow");
+		localStorage.removeItem('back');
 	});
-	$("body").on("swiperight", function(event) {
-		console.log("swipe right");
-		$("div[data-role='content']").animate({scrollLeft: pos + 200} );
-	});
-	*/
 	$(document).on('pagechange', function(event, data) {
 		//console.log("pagechange");
-		//console.log("url: " + data.absUrl);
-		//console.log("LS.href: " + localStorage.href);
-		//console.log("LS.back: " + localStorage.back);
-		//console.log("LS.rodzaj: "+localStorage.rodzaj);
 		var url = data.absUrl.replace("#", "").split("?");
+		Site.params={};
 		var params = {};
 		if (url[1]) {
 			var parameters = url[1].split("&");
@@ -77,71 +108,12 @@ Site.init = function() {
 				console.log("Param " + s[0] + ": " + s[1]);
 			}
 		}
-		var plik = plikURL(data.absUrl);
-		Site.id_atrakcji = params.id;
-		if ($("select#rodzaj").length != 0 && $("select#rodzaj option").length == 0) {
-			var option = $("<option value=''>Wszystkie</option>");
-			$("select#rodzaj").append(option);
-			for (var r in Site.rodzaje) {
-				if (Site.atrakcjeWgRodzajow[r]) {
-					var option = $("<option value='" + r + "'>" + Site.rodzaje[r] + " (" + Site.atrakcjeWgRodzajow[r].length + ")</option>");
-					$("select#rodzaj").append(option);
-				}
-			}
-			if (localStorage.back) {
-				if (localStorage.rodzaj)
-					$("select#rodzaj").val(localStorage.rodzaj);
-				if (localStorage.nazwa)
-					$("input#nazwa").val(localStorage.nazwa);
-			} else {
-				if (params.rodzaj)
-					$("select#rodzaj").val(params.rodzaj);
-			}
-			$("select#rodzaj").selectmenu('refresh');
-			$("select#rodzaj").change(function() {
-				Site.filtrujWyniki();
-			});
-		}
-		$("input#nazwa").keyup(function() {
-			Site.filtrujWyniki();
-		});
-		if (params.hero) {
-			$(".ui-page-active .fullScreen").css("background-image", "url(" + Site.atrakcje[params.hero].zdjecia.hero + ")");
-		}
-		if (Site.id_atrakcji) {
-			Site.wczytajDaneAtrakcji();
-		}
-		if (plik == 'atrakcja.html') {
-			$(".hero.klik").click(function() {
-				$.mobile.navigate("hero.html?hero=" + Site.id_atrakcji);
-			});
-		} else if (plik == "atrakcje.html") {
-			if ($("#atrakcjeListaAtrakcji").length > 0 && $("#atrakcjeListaAtrakcji li").length == 0) {
-				Site.generateList("atrakcjeListaAtrakcji");
-				Site.filtrujWyniki();
-			}
-		}
-		localStorage.removeItem('back');
-		resize();
+		Site.params=params;
 	});
-
-	$(window).resize(function() {
-		if ($(".ui-page-active div.tresc .txt").length > 0) {
-			Site.przeladujAtrakcje();
-		} else {
-			resize();
-		}
-	});
-	resize();
 };
 $(window).on("navigate", function(event, data) {
-	var direction = data.state.direction;
-	if (direction == 'back') {
+	if (data.state.direction == 'back') {
 		localStorage.back = 1;
-	}
-	if ($("#mainListaAtrakcji").length > 0) {
-		var obj = $("#mainListaAtrakcji").parent();
-		$("form.ui-listview-filter .ui-input-clear", obj).trigger("click");
 	}
 });
 Site.wczytajDaneAtrakcji = function() {
@@ -178,26 +150,16 @@ Site.pokazAtrakcje = function() {
 	var img = $("<img src='" + this.atrakcja.zdjecia.glowne + "'/>");
 	$(".ui-page-active div#zdjecie").append(img);
 	if (this.atrakcja.szer_geogr) {
-		this.mapaGoogle();
+		var a = $("<a></a>");
+		$(a).attr("href", "geo:" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "?z=15&q=" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "(" + this.atrakcja.tytul + ")");
+		var imgSrc = "http://maps.googleapis.com/maps/api/staticmap?center=" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "&zoom=15&size=400x300&maptype=roadmap&markers=color:blue%7C" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "&sensor=false";
+		var img = $("<img src='" + imgSrc + "'/>");
+		$(a).append(img);
+		$("#google_maps").append(a);
 	}
 };
-Site.przeladujAtrakcje = function() {
-	$(".ui-page-active div.tresc .txt").html("");
-	$(".ui-page-active div.tresc").parent().parent().width('300px');
-	$(".ui-page-active div.tresc").attr("style", "-webkit-column-count:" + 1);
-	$("#google_maps").html("");
-	this.pokazAtrakcje();
-};
-Site.mapaGoogle = function() {
-	var a = $("<a></a>");
-	$(a).attr("href", "geo:" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "?z=15&q=" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "(" + this.atrakcja.tytul + ")");
-	var imgSrc = "http://maps.googleapis.com/maps/api/staticmap?center=" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "&zoom=15&size=400x300&maptype=roadmap&markers=color:blue%7C" + this.atrakcja.szer_geogr + "," + this.atrakcja.dl_geogr + "&sensor=false";
-	var img = $("<img src='" + imgSrc + "'/>");
-	$(a).append(img);
-	$("#google_maps").append(a);
-};
 Site.generatePolecamy = function() {
-	var cel="polecamy";
+	var cel = "polecamy";
 	for (a in this.atrakcje) {
 		if (this.atrakcje[a].wyrozniony) {
 			var inner = $('<img src="' + this.atrakcje[a].zdjecia.lista + '"> ' + '<h2>' + this.atrakcje[a].tytul + '</h2>');
@@ -237,6 +199,7 @@ Site.filtrujWyniki = function() {
 		localStorage.rodzaj = rodzaj;
 		$("#atrakcjeListaAtrakcji li").filter(":not([data-rodzaj='" + rodzaj + "'])").hide();
 	}
+	console.log("LS.rodzaj: "+localStorage.rodzaj);
 };
 Site.wczytajDane = function() {
 	$.mobile.loading("show", {
@@ -251,63 +214,22 @@ Site.wczytajDane = function() {
 	}).done(function(dane) {
 		localStorage.atrakcje = JSON.stringify(dane);
 		Site.atrakcje = dane;
-		for (var d in dane) {
-			var atr = dane[d];
-			if (!Site.atrakcjeWgRodzajow[atr.rodzaj])
-				Site.atrakcjeWgRodzajow[atr.rodzaj] = [];
-			Site.atrakcjeWgRodzajow[atr.rodzaj].push(d);
-		}
+		Site.przygotujDane();
 		Site.generatePolecamy();
 		Site.generateList();
 		$.mobile.loading("hide");
 	});
 };
+Site.przygotujDane=function () {
+for (var d in Site.atrakcje) {
+		var atr = Site.atrakcje[d];
+		if (!Site.atrakcjeWgRodzajow[atr.rodzaj])
+			Site.atrakcjeWgRodzajow[atr.rodzaj] = [];
+		Site.atrakcjeWgRodzajow[atr.rodzaj].push(d);
+	}
+};
 
-function resize() {
-	return;
-	var szer = 0;
-	var wysOkna = $(window).height();
-	$(".ui-page-active div[data-role='okno']").each(function(index) {
-		szer += $(this).outerWidth(true);
-	});
-	$("div[data-role='ekran']").width(szer + 'px');
-	$("div[data-role='wnetrze']").height((wysOkna - $("div[data-role='okno'] h2").outerHeight(true) - 10) + 'px');
-}
-
-function plikURL() {
-	var loc = window.location;
-	var pathName = loc.pathname.substring(loc.pathname.lastIndexOf('/') + 1);
-	return pathName;
-}
 
 jQuery.expr[':'].Contains = function(a, i, m) {
 	return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 };
-$.event.special.swipe = $.extend($.event.special.swipe, {
-	start : function(event) {
-		var data = event.originalEvent.touches ? event.originalEvent.touches[0] : event;
-		return {
-			time : (new Date() ).getTime(),
-			coords : [data.pageX, data.pageY],
-			origin : $(event.target),
-			offset : $('body').scrollTop()
-		};
-	},
-
-	stop : function(event) {
-		var data = event.originalEvent.touches ? event.originalEvent.touches[0] : event;
-		return {
-			time : (new Date() ).getTime(),
-			coords : [data.pageX, data.pageY],
-			offset : $('body').scrollTop()
-		};
-	},
-
-	handleSwipe : function(start, stop) {
-		var swipe = $.event.special.swipe, x = Math.abs(start.coords[0] - stop.coords[0]), y = Math.abs(start.coords[1] - stop.coords[1]), offset = Math.abs(start.offset - stop.offset), time = stop.time - start.time;
-		if (time < swipe.durationThreshold && x > swipe.horizontalDistanceThreshold && (y + offset ) < swipe.verticalDistanceThreshold) {
-
-			start.origin.trigger("swipe").trigger((start.coords[0] - stop.coords[0] ) > 0 ? "swipeleft" : "swiperight");
-		}
-	}
-}); 
